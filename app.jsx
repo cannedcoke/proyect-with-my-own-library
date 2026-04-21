b789d4803fb3610f68ac9991923f4a1710f3a7f3
Cyanide <miqueas736d@gmail.com>
2026-04-21 13:18:33 -0300

    add: adding base structure to start the proyect with cyanide library

---
+++ app.jsx
@@ -0,0 +1,13 @@
+/** @jsx cyanide */
+import { cyanide, mount, setState, getState } from 'cyanide';
+// import {Tarea} from "./components/Tarea"
+const App = () => {
+  // const { count = 0 } = getState();
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+mount(App, '#app');
